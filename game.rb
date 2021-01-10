af571d98a4ee9f183d931044a91cced18b2ce300
Jake Dam <robert.dam2@gmail.com>
2021-01-10 09:58:18 -0500

    fixed bug in find_num_matches, wrote game_over

--- game.rb
+++ game.rb
@@ -9,7 +9,7 @@ class Game
   def find_exact_matches(guess, code, array)
     exact_matches_found = 0
     guess.each_with_index do |guess_num, guess_index|
-      if guess[guess_index] == code[guess_index]
+      if guess_num == code[guess_index]
         exact_matches_found += 1
         array << guess_index
       end
@@ -17,7 +17,6 @@ class Game
     exact_matches_found
   end
 
-  #need to debug
   def find_num_matches(guess, code, array)
     num_matches_found = 0
     guess.each do |guess_num|
@@ -25,17 +24,28 @@ class Game
         if guess_num == code_num && array.include?(code_index) == false
           num_matches_found += 1
           array << code_index
+          break
         end
       end
     end
-    p num_matches_found
-    p array
     num_matches_found
+  end
+
+  def game_over(outcome)
+    if outcome == "win"
+      puts "Code sucessfully cracked! Congrats!"
+      exit
+    else
+      puts "You failed to crack the code!"
+      exit
+    end
   end
 
   def play
     code_generator
-    code = @computer.generate_code.to_s.split("")
+    @computer.generate_code
+    code = ["4", "1", "1", "2"] #@computer.comp_code.map { |num| num.to_s }
+    p code
     while @turns < 13 do
       puts "Turn #{@turns}: #{@player.name}, enter your guess"
       guess = @player.make_guess.split("")
@@ -43,8 +53,7 @@ class Game
       print "\n"
       if guess == code
         feedback(4,0)
-        #Game over, add player wins method here
-        puts "Congrats you cracked the code!!"
+        game_over("win")
       end
       exact_matches, num_matches = 0, 0
       match_indexes = []
@@ -53,5 +62,6 @@ class Game
       feedback(exact_matches, num_matches)
       @turns += 1
     end
+    game_over("lose")
   end
 end
