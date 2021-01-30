60606b3729897b637821cb2025b7be963438db1f
Jake Dam <robert.dam2@gmail.com>
2021-01-30 09:37:00 -0500

    refactored comp_solve

--- computer.rb
+++ computer.rb
@@ -19,32 +19,65 @@ class Computer
     guess
   end
 
-  def comp_solve(exact_matches, color_matches)
-    feedback = exact_matches + color_matches
-    guess = @next_guess
-    case feedback
-    when 0
-      next_num = @next_guess[0].to_i + 1
-      guess = guess.map { |_n| n = next_num.to_s }
-    when 1
-      next_num = @next_guess[1].to_i + 1
-      guess = guess.map.with_index { |n, i| i == 0 ? n : n = next_num.to_s }
-    when 2
-      next_num = @next_guess[2].to_i + 1
-      guess = guess.map.with_index { |n, i| [0, 1].include?(i) ? n : n = next_num.to_s }
-    when 3
-      next_num = @next_guess[3].to_i + 1
-      guess[3] = next_num.to_s
-    when 4
-      case exact_matches
-      when 0
-        guess = guess.shuffle
-      when 1
-        guess[1, 3] = guess[1, 3].shuffle
-      when 2
-        guess[2, 3] = guess[2, 3].shuffle
-      end
-    end
-    @next_guess.replace(guess)
+  def feedback0(guess)
+    next_num = @next_guess[0].to_i + 1
+    guess.map { next_num.to_s }
   end
+
+  def feedback1(guess)
+    next_num = @next_guess[1].to_i + 1
+    guess.map.with_index { |n, i| i.zero? ? n : next_num.to_s }
+  end
+
+  def feedback2(guess)
+    next_num = @next_guess[2].to_i + 1
+    guess.map.with_index { |n, i| [0, 1].include?(i) ? n : next_num.to_s }
+  end
+
+  def feedback3(guess)
+    next_num = @next_guess[3].to_i + 1
+    guess[3] = next_num.to_s
+    guess
+  end
+
+  def feedback4(guess, exact_matches)
+    case exact_matches
+    when 0
+      guess = guess.shuffle
+    when 1
+      guess[1, 3] = guess[1, 3].shuffle
+    when 2
+      guess[2, 3] = guess[2, 3].shuffle
+    end
+    guess
+  end
+
+  #   def comp_solve(exact_matches, color_matches)
+  #     feedback = exact_matches + color_matches
+  #     guess = @next_guess
+  #     case feedback
+  #     when 0
+  #       next_num = @next_guess[0].to_i + 1
+  #       guess = guess.map { |_n| n = next_num.to_s }
+  #     when 1
+  #       next_num = @next_guess[1].to_i + 1
+  #       guess = guess.map.with_index { |n, i| i == 0 ? n : n = next_num.to_s }
+  #     when 2
+  #       next_num = @next_guess[2].to_i + 1
+  #       guess = guess.map.with_index { |n, i| [0, 1].include?(i) ? n : n = next_num.to_s }
+  #     when 3
+  #       next_num = @next_guess[3].to_i + 1
+  #       guess[3] = next_num.to_s
+  #     when 4
+  #       case exact_matches
+  #       when 0
+  #         guess = guess.shuffle
+  #       when 1
+  #         guess[1, 3] = guess[1, 3].shuffle
+  #       when 2
+  #         guess[2, 3] = guess[2, 3].shuffle
+  #       end
+  #     end
+  #     @next_guess.replace(guess)
+  #   end
 end
